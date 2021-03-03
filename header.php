40ba78cd00f634d08c24b1f132c06144a11e6c13
The Hunger Project <john.coonrod@thp.org>
2021-03-03 10:18:59 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * The header for the theme
+ * Adapted by JC to add search window, logo, subtitle, responsive hamburger based on thptheme
  *
+ */
+
+?>
+<?php
+/**
+ * The header for the theme
  */
 
 ?>
@@ -10,26 +16,35 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Unna|Source+Sans+Pro">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+	<script>
+		function thpmobile() {
+			document.getElementById('menu-primary').style.display='block';
+		}
+		function thpsearch() {
+			document.getElementById('thp-search').style.display='block';			
+		}
+	</script>
 	
 <a class="screen-reader-text" href="#content">Skip to content</a>
 
 <header class="site-header">
-	<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+	<?php the_custom_logo();?>
 
 	<nav class="main-navigation">
-		<?php
-		wp_nav_menu( array(
-			'theme_location' => 'menu-1',
-			'menu_id'        => 'primary-menu',
-		) );
-		?>
+		<?php wp_nav_menu( array('theme_location' => 'menu-1') ); ?>
 	</nav>
+	<a id="hamburger" 
+			href="javascript:thpmobile();"><span class="dashicons dashicons-menu-alt3"></span>&nbsp;</a>
 </header>
+		<div id='thp-search' style='text-align:center;display:none;'>
+			<?php get_search_form();?>
+		</div>
 
 <div id="content" class="site-content">
 	
