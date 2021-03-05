13822607003ba51b0c757c8bc322205552018a94
The Hunger Project <john.coonrod@thp.org>
2021-03-05 16:58:55 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,6 +19,22 @@ function add_search_form($items, $args) {
 }
 add_filter('wp_nav_menu_items', 'add_search_form', 10, 2);
 
+//Remove JQuery migrate
+function remove_jquery_migrate($scripts)
+{
+    if (!is_admin() && isset($scripts->registered['jquery'])) {
+        $script = $scripts->registered['jquery'];
+        
+        if ($script->deps) { // Check whether the script has any dependencies
+            $script->deps = array_diff($script->deps, array(
+                'jquery-migrate'
+            ));
+        }
+    }
+}
+
+add_action('wp_default_scripts', 'remove_jquery_migrate');
+
 
 /* custom colors
 */
